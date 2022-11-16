67afbf5d374d1e2aa02c8b33f0e262e7a0ef27fb
FerminCho <fermin.liljeqvist@gmail.com>
2022-11-16 23:32:05 +0100

    added front page draft1

---
+++ content/Front page/Front page.vue
@@ -0,0 +1,15 @@
+<style>
+.sponsor1 {
+    display: inline-block;
+} 
+</style>
+
+<template>
+    <div class="info">
+        <p>Explaination how to use the catalog</p>
+    </div>
+    <div class="sponsors">
+        <div class="sponsor1"><img src="sponsorLogo.jpg" alt="logo"></div>
+        <div class="sponsor1"><a href="www.sponsor.se">Sponsor</a></div>
+    </div>
+</template>
