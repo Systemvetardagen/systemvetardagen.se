58e1145027935e0b796c4dcf4d2d1233a451af90
Yizhou Yang <yangyizhou99@gmail.com>
2022-11-29 23:38:56 +0100

    fea: svg concept map

---
+++ pages/map.vue
@@ -0,0 +1,117 @@
+<template>
+  <section>
+    <div>
+      <svg
+   width="300mm"
+   height="250mm"
+   viewBox="0 0 300 250"
+   version="1.1"
+   id="svg5"
+   inkscape:version="1.2.1 (1:1.2.1+202210291243+9c6d41e410)"
+   sodipodi:docname="map.svg"
+   xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
+   xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
+   xmlns="http://www.w3.org/2000/svg"
+   xmlns:svg="http://www.w3.org/2000/svg">
+  <sodipodi:namedview
+     id="namedview7"
+     pagecolor="#ffffff"
+     bordercolor="#000000"
+     borderopacity="0.25"
+     inkscape:showpageshadow="2"
+     inkscape:pageopacity="0.0"
+     inkscape:pagecheckerboard="0"
+     inkscape:deskcolor="#d1d1d1"
+     inkscape:document-units="mm"
+     showgrid="false"
+     inkscape:zoom="2.1417558"
+     inkscape:cx="283.41233"
+     inkscape:cy="768.29487"
+     inkscape:window-width="1846"
+     inkscape:window-height="1016"
+     inkscape:window-x="74"
+     inkscape:window-y="27"
+     inkscape:window-maximized="1"
+     inkscape:current-layer="layer1" />
+  <defs
+     id="defs2">
+    <inkscape:path-effect
+       effect="spiro"
+       id="path-effect294"
+       is_visible="true"
+       lpeversion="1" />
+    <inkscape:path-effect
+       effect="spiro"
+       id="path-effect126"
+       is_visible="true"
+       lpeversion="1" />
+  </defs>
+  <g
+     inkscape:label="Layer 1"
+     inkscape:groupmode="layer"
+     id="layer1">
+    <path
+       style="fill:#ff0000;stroke:#ff0000;stroke-width:0.264583"
+       d="M 44.732553,227.71216 H 266.50585"
+       id="path124"
+       inkscape:path-effect="#path-effect126"
+       inkscape:original-d="M 44.732553,227.71216 H 266.50585" />
+    <path
+       style="fill:none;stroke:#ff0000;stroke-width:0.264583"
+       d="M 34.59571,211.8826 203.41651,43.061792"
+       id="path290" />
+    <path
+       style="fill:none;stroke:#ff0000;stroke-width:0.264583"
+       d="m 35.070811,211.45761 c -1.24191,1.37584 -2.085437,3.1072 -2.403462,4.93316 -0.318024,1.82597 -0.109505,3.74056 0.594133,5.45525 0.90139,2.19659 2.621721,4.04593 4.747519,5.10352 2.125797,1.0576 4.63851,1.31422 6.934214,0.7082"
+       id="path292"
+       inkscape:path-effect="#path-effect294"
+       inkscape:original-d="m 35.070811,211.45761 c -0.563678,3.46307 -1.205955,6.92587 -1.809329,10.38841 -0.603374,3.46254 7.788087,3.87474 11.681733,5.81172" />
+    <ellipse
+       style="fill:#ff0000;stroke:none;stroke-width:0.264583"
+       id="path504"
+       cx="49.077484"
+       cy="222.41498"
+       rx="1.0342007"
+       ry="1.1215688" />
+    <a href="/"><text
+       xml:space="preserve"
+       style="font-size:2.11667px;font-family:'DejaVu Sans Mono';-inkscape-font-specification:'DejaVu Sans Mono';fill:#00ffff;stroke:none;stroke-width:0.264583"
+       x="48.417408"
+       y="223.23935"
+       id="text894"><tspan
+         sodipodi:role="line"
+         id="tspan892"
+         style="stroke-width:0.264583"
+         x="48.417408"
+         y="223.23935">1</tspan></text></a>
+    <path
+       style="fill:#00ffff;stroke:#ff0000;stroke-width:0.264583"
+       d="m 36.900008,204.90292 5.122045,5.12205"
+       id="path304" />
+    <path
+       style="fill:#00ffff;stroke:#ff0000;stroke-width:0.264583"
+       d="m 42.613426,199.52804 4.954206,4.95421"
+       id="path306" />
+    <text
+       xml:space="preserve"
+       style="font-size:2.11667px;font-family:'DejaVu Sans Mono';-inkscape-font-specification:'DejaVu Sans Mono';fill:#00ffff;stroke:#ff0000;stroke-width:0.264583"
+       x="23.550398"
+       y="195.14897"
+       id="text232"><tspan
+         sodipodi:role="line"
+         id="tspan230"
+         style="stroke-width:0.264583"
+         x="23.550398"
+         y="195.14897">Huvudentre</tspan><tspan
+         sodipodi:role="line"
+         style="stroke-width:0.264583"
+         x="23.550398"
+         y="197.79482"
+         id="tspan234">/Main Entrance</tspan></text>
+  </g>
+</svg>
+
+    </div>
+  </section>
+</template>
+
