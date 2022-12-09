1b5587fa2f4666c591f289730cfefb0af6000f04
olehoef <ole.hoefener@t-online.de>
2022-12-09 17:45:59 +0100

    hero_background-img-opacity

--- pages/index-subviews/hero.vue
+++ pages/index-subviews/hero.vue
@@ -1,16 +1,19 @@
 <template>
     <div id="home" class="hero">
 
-        <!-- Hero Text -->
-        <div>
-            <h1 class="hero-heading" style="color: var(--clr-blue-600)" >Find your next job</h1>
-            <h3 style="color: var(--clr-blue-900)">Sveriges största arbetsmarknadsdag för studenter och arbetsgivare inom IT-branschen!</h3>
-        </div>
+        <div class="hero-content">
+             <!-- Hero Text -->
+            <div>
+                <h1 class="hero-heading" style="color: var(--clr-blue-600)" >Find your next job</h1>
+                <h3 style="color: var(--clr-blue-900)">Sveriges största arbetsmarknadsdag för studenter och arbetsgivare inom IT-branschen!</h3>
+            </div>
 
         <!-- Hero Buttons -->
-        <div class="hero-buttons">
-            <Button title="Katalog" link="" bColor="gradient"/>
-            <Button title="Kontakt oss" link="" bColor="--clr-white" tColor="--clr-blue"/>
+            <div class="hero-buttons">
+                <Button title="Katalog" link="" bColor="gradient"/>
+                <Button title="Kontakt oss" link="" bColor="--clr-white" tColor="--clr-blue"/>
+            </div>
+
         </div>
 
         
@@ -33,13 +36,25 @@
 <style>
     .hero {
         height: 100vh;
-        background: linear-gradient(rgba(27, 40, 51, 0.2666666667), rgba(27, 40, 51, 0.2666666667), var(--clr-darkblue)), url("@/assets/img/hero-img-provisional.png");
-        background-size: cover;
         background-position: center;
         background-repeat: no-repeat;
-        background-size: cover;
         justify-content: flex-end;
         
+    }
+
+    .hero::before {    
+      content: "";
+      background-image: url('@/assets/img/hero-img-provisional.png');
+      background-size: cover;
+      position: absolute;
+      top: 0px;
+      right: 0px;
+      bottom: 0px;
+      left: 0px;
+      opacity: 0.75;
+}
+    .hero-content {
+        position: relative;
     }
 
     
