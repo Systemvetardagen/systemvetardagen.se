bda01e61163c0cdcaee8698ca7355c9b7f2f6446
aakruti27 <52568409+aakruti27@users.noreply.github.com>
2023-01-05 18:11:23 +0100

    prevent overlap info (AM)

--- pages/index-subviews/info.vue
+++ pages/index-subviews/info.vue
@@ -1,23 +1,25 @@
 <template>
-    <section class="info">
-        <div class="info-second">
-			<h3 style="color:var(--clr-blue-600); text-align: center;">{{ $t('during_the_fair') }}</h3>
-			<p><ul>
-				<li>{{ $t('during_the_fair_description1') }}</li>
-				<li>{{ $t('during_the_fair_description2') }}</li>
-				<li>{{ $t('during_the_fair_description3') }}</li>
-			</ul></p>
-		</div>
-		<div class="info-first">
-			<h3 style=" color:var(--clr-pink-600); text-align: center;">{{ $t('before_the_fair') }}</h3>
-			<p><ul>
-				<li>{{ $t('before_the_fair_description1') }}</li>
-				<li>{{ $t('before_the_fair_description2') }}</li>
-				<li>{{ $t('before_the_fair_description3') }}</li>
-				<li>{{ $t('before_the_fair_description4') }}</li>
-			</ul></p>
-			<p>{{ $t('before_the_fair_description5') }}</p>
-		</div>
+    <section>
+        <div id="info">
+            <div class="info-second">
+                <h3 style="color:var(--clr-blue-600); text-align: center;">{{ $t('during_the_fair') }}</h3>
+                <p><ul>
+                    <li>{{ $t('during_the_fair_description1') }}</li>
+                    <li>{{ $t('during_the_fair_description2') }}</li>
+                    <li>{{ $t('during_the_fair_description3') }}</li>
+                </ul></p>
+            </div>
+            <div class="info-first">
+                <h3 style=" color:var(--clr-pink-600); text-align: center;">{{ $t('before_the_fair') }}</h3>
+                <p><ul>
+                    <li>{{ $t('before_the_fair_description1') }}</li>
+                    <li>{{ $t('before_the_fair_description2') }}</li>
+                    <li>{{ $t('before_the_fair_description3') }}</li>
+                    <li>{{ $t('before_the_fair_description4') }}</li>
+                </ul></p>
+                <p>{{ $t('before_the_fair_description5') }}</p>
+            </div>
+        </div>
 
 	</section>
 
@@ -45,8 +47,9 @@
         background-size: cover;
         height: 100vh;
         background-repeat: no-repeat;
-        margin-bottom: 10rem;
+        margin-bottom: 0rem;
         margin-top: 0rem;
+        padding-bottom:2%;
     }
     .info-first{
         margin-left: 0rem;
@@ -54,12 +57,5 @@
     .info-second{
         margin-left: 0rem;
     }
-    .vectorinfo{
-        position: relative;
-        left: -10.07%;
-        right: 44.58%;
-        top: 45.9%;
-        bottom: -23.73%;
-        background: linear-gradient(0deg, rgba(255, 243, 249, 0.82), rgba(255, 243, 249, 0.82)), url('assets\img\nod_11.jpg');
-    }
+
 </style>
