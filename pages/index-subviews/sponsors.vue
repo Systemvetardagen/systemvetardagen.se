51b0baba50316cd91d6facb101ca284a4ab0879e
olehoef <ole.hoefener@t-online.de>
2023-01-05 18:00:39 +0100

    sponsors_implement-page
    The sponsor page is implemented. However, there is no content yet. So far, "coming soon..." is displayed.

---
+++ pages/index-subviews/sponsors.vue
@@ -0,0 +1,23 @@
+<template>
+    <section class="sponsors">
+        <h2 sytle="color: var(--clr-blue-700);"> {{$t('sponsors')}}</h2>
+
+        <h3>Coming soon...</h3>
+    </section>
+
+</template>
+
+<script>
+    import Button from '@/components/Button.vue'
+
+    export default {
+        name: 'heroSection'
+    }
+    components: {
+        Button
+    }
+
+</script>
+<style>
+    
+</style>
