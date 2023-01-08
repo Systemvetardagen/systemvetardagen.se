af2f41bc7d7b90c34bbecf5906d017e60046aed2
FerminCho <fermin.liljeqvist@gmail.com>
2023-01-08 18:59:52 +0100

    Update index.vue

--- pages/Catalog/index.vue
+++ pages/Catalog/index.vue
@@ -2,7 +2,7 @@
   <main>
     <section v-if="posts" class="w-full max-w-5xl mx-auto">
       <h1 class="title">Company</h1>
-      <posts post-type="company" :amount="10" />
+      <posts post-type="Company_pages" :amount="10" />
     </section>
   </main>
 </template>
