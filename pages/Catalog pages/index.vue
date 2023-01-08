89de6923a3a5147e9821dd300db1c52841059823
FerminCho <fermin.liljeqvist@gmail.com>
2023-01-08 18:39:59 +0100

    testing generating pages

---
+++ pages/Catalog pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <main>
+    <section v-if="posts" class="w-full max-w-5xl mx-auto">
+      <h1 class="title">Company</h1>
+      <posts post-type="Company" :amount="10" />
+    </section>
+  </main>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content, error }) {
+    let posts;
+    try {
+      posts = await $content("Company_pages").fetch();
+    } catch (e) {
+      error({ message: "Blog posts not found" });
+    }
+    return { posts };
+  },
+}
+</script> 
