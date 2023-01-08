5e25576a0e286dd4fde7c88b3e6832a30184c49f
FerminCho <fermin.liljeqvist@gmail.com>
2023-01-08 11:29:30 +0100

    Update Company page.vue

--- pages/Catalog pages/Company page.vue
+++ pages/Catalog pages/Company page.vue
@@ -1,13 +1,19 @@
-
+<template>
+  <main>
+    <section v-if="post">
+      <h1 class="">{{ post.title }}</h1>
+    </section>
+  </main>
+</template>
 
 <script>
 export default {
   async asyncData({ $content, params, error }) {
     let post;
     try {
-      post = await $content("Company_pages", params.blog).fetch();
+      post = await $content("Company_pages", params.Company_pages).fetch();
     } catch (e) {
-      error({ message: "Blog post not found" });
+      error({ message: "Entry not found" });
     }
     return { post };
   }
