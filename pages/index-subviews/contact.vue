ff1c56d415448a318521f708e414ed6f290bc774
olehoef <ole.hoefener@t-online.de>
2023-01-16 16:28:14 +0100

    multiple
    Import and adapt css

--- pages/index-subviews/contact.vue
+++ pages/index-subviews/contact.vue
@@ -2,16 +2,16 @@
    
     <section  class="contact">
                  <form class="contact-form">
-                     <h2 class="text-blue">{{ $t('contact_us') }}</h2>
+                     <h2 style="color: var(--clr-grey-1000)" >{{ $t('contact_us') }}</h2>
                      <div class="inputs">
-                         <div class="inputs-left-col">
+                         
                              
-                             <input class="form-name" type="text" placeholder="name" name="name" required>
-                             <input class="form-email" type="email" placeholder="email" name="email" required>
-                             <input class="form-company" type="text" placeholder="company" name="company" required>
-                             <input autocomplete="off" type="text" id="form-title" name="title" placeholder="Title"
-                                 style="display: none">
-                         </div>
+                        <input type="text" placeholder="name" name="name" required>
+                        <input type="email" placeholder="email" name="email" required>
+                        <input type="text" placeholder="company" name="company" required>
+                        <input autocomplete="off" type="text" id="form-title" name="title" placeholder="Title"
+                            style="display: none">
+
                          <textarea class="form-message" name="message" placeholder="message" rows="3"
                              style="resize: none;"></textarea>
                      </div>
@@ -25,3 +25,85 @@ export default {
     name: 'contact'
 }
 </script>
+<style>
+.contact {
+  padding-top: 0;
+  display: flex;
+  flex-direction: column;
+
+
+}
+
+.contact-form {
+  display: flex;
+  flex-direction: column;
+
+   border-radius: nullpx;
+   text-align: left;
+
+/* Black/900 */
+   color: #303030;
+
+
+/* Inside auto layout */
+   flex: none;
+   order: 0;
+   flex-grow: 0;
+
+}
+
+.inputs {
+  display: flex;
+  flex-direction: column;
+
+
+}
+
+
+@media (min-width: 1024px) {
+  .inputs {
+    flex-direction: column;
+  }
+  .inputs-left-col {
+    margin-right: 1rem;
+  }
+}
+input,
+textarea {
+  margin: 0;
+  margin-bottom: 1rem;
+  padding: 0.8rem 1rem;
+  width: 24rem;
+  max-width: 100%;
+  background-color: white;
+  border-radius: 0;
+  color: var(--clr-grey-1000);
+  /* transition: border-color 0.2s; */
+}
+
+input:focus,
+textarea:focus {
+  /* border-color: var(--clr-blue); */
+  outline: none;
+}
+
+input::-moz-placeholder, textarea::-moz-placeholder {
+  color: var(--clr-grey-1000);
+}
+
+input:-ms-input-placeholder, textarea:-ms-input-placeholder {
+  color: var(--clr-grey-1000);
+}
+
+input::placeholder,
+textarea::placeholder {
+  color: var(--clr-grey-1000);
+}
+
+.button-form {
+  margin-top: 0;
+  width: 12rem;
+  align-self: flex-end;
+}
+
+</style>
