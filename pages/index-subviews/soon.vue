ca92dd832a9a375cc23caadb591845c71688a50d
olehoef <ole.hoefener@t-online.de>
2023-01-18 17:12:35 +0100

    bg-alignment-changes

--- pages/index-subviews/soon.vue
+++ pages/index-subviews/soon.vue
@@ -1,7 +1,7 @@
 <template>
     <section class="soon">
         <div class="soon-text">
-            <h2 style="color:var(--clr-pink-600); text-align: left; ">{{ $t('it_is_almost_time!') }}</h2>
+            <h2 style="color:var(--clr-pink-600); text-align: center; ">{{ $t('it_is_almost_time!') }}</h2>
             <p class="p" style="color: black;">{{ $t('disk_introduction1') }}<a
             class="inline-link" style="color: black;" href=https://disk.su.se>{{ $t('disk') }}</a>
             {{ $t('disk_introduction2') }}
@@ -28,7 +28,12 @@
         display: flex;
         justify-content: center;
         /* background:  linear-gradient(0deg, rgba(255, 255, 255, 0.4), rgba(255, 255, 255, 0.4)), url('assets/img/soon-bg-2023.png'); */
-        background: url('assets/img/soon-bg-2023.png');
+        background: url('assets/img/soon-bg-2023.png'); 
+        background-position-x: 0% 0%;
+        /* -webkit-background-size: cover;
+        -moz-background-size: cover;
+        -o-background-size: cover; */
+        background-size: cover;
         background-position: center;
         background-size: cover;
         /* height: 100vh; */
@@ -38,6 +43,9 @@
     .soon-text{
         /* padding:1rem; */
         /* margin-left: 3rem; */
+        background: white;
+        padding: 1rem 2rem;
+        border-radius: 1rem;
       
     }
     @media (max-width: 600px) {
@@ -47,7 +55,7 @@
     }
     @media (min-width: 601px) {
     .soon-text {
-    max-width: 40ch;
+    max-width: 43ch;
     }
     }
 </style>
