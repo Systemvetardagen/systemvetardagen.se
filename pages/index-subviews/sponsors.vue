c31bc400f6cd165801e9ebcff5ca24a0d2fd8daa
Joakim Wirén <47276887+j-wiren@users.noreply.github.com>
2023-01-25 16:50:48 +0100

    Blob CSS placement example

--- pages/index-subviews/sponsors.vue
+++ pages/index-subviews/sponsors.vue
@@ -1,37 +1,40 @@
 <template>
-    <div class="sponsors">
-        <div class="sponsor-content">
-            <h2 style="color: var(--clr-blue-700);"> {{$t('sponsors')}}</h2>
-
-            <h3>Coming soon...</h3>
-
-        </div>
-        
+  <div class="sponsors">
+    <div class="sponsor-content">
+      <h2 style="color: var(--clr-blue-700)">{{ $t("sponsors") }}</h2>
+      <h3>Coming soon...</h3>
     </div>
-
+    <img src="@/assets/img/blobs/Blob-5-b.svg" alt="blob" class="blob" />
+  </div>
 </template>
 
 <script>
-    import Button from '@/components/Button.vue'
+import Button from "@/components/Button.vue";
 
-    export default {
-        name: 'heroSection'
-    }
-    components: {
-        Button
-    }
-
+export default {
+  name: "heroSection",
+};
+components: {
+  Button;
+}
 </script>
 <style>
-    .sponsors {
-        height: 100vh;
-        display: flex;
-        justify-content: center;
-        padding: 5%;
+.sponsors {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  padding: 5%;
+}
 
-    }
+.sponsor-content {
+  z-index: 100;
+}
 
-    .sponsor-conent {
-    
-    }
+.blob {
+  height: 30rem;
+  position: absolute;
+  z-index: 1;
+  left: 0;
+  transform: translateX(-50%);
+}
 </style>
