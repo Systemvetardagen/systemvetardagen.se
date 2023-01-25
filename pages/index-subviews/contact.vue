3f290a95cdeec64eb5607556a744534365471180
Joakim Wirén <47276887+j-wiren@users.noreply.github.com>
2023-01-25 15:02:07 +0100

    Field padding adjustment + formatting

--- pages/index-subviews/contact.vue
+++ pages/index-subviews/contact.vue
@@ -1,108 +1,123 @@
 <template>
-   
-    <section  class="contact">
-                 <form class="contact-form" id="contact-form" @submit.prevent="submit">
-                     <h2 style="color: var(--clr-grey-1000)" >{{ $t('contact-form-title') }}</h2>
-                     <div class="inputs">
-                         
-                             
-                        <input 
-                            type="text" 
-                            :placeholder="this.$t('contact-form-name')" 
-                            name="name" 
-                            v-model="name"
-                            required>
-                        <input 
-                            type="email" 
-                            :placeholder="this.$t('contact-form-email')" 
-                            name="email" 
-                            v-model="email"
-                            required>
-                        <input 
-                            type="text" 
-                            :placeholder="this.$t('contact-form-company')" 
-                            name="company" 
-                            v-model="company"
-                            required>
+  <section class="contact">
+    <form class="contact-form" id="contact-form" @submit.prevent="submit">
+      <h2 style="color: var(--clr-grey-1000)">
+        {{ $t("contact-form-title") }}
+      </h2>
+      <div class="inputs">
+        <input
+          type="text"
+          :placeholder="this.$t('contact-form-name')"
+          name="name"
+          v-model="name"
+          required
+        />
+        <input
+          type="email"
+          :placeholder="this.$t('contact-form-email')"
+          name="email"
+          v-model="email"
+          required
+        />
+        <input
+          type="text"
+          :placeholder="this.$t('contact-form-company')"
+          name="company"
+          v-model="company"
+          required
+        />
 
-                        <input v-model="botmsg" autocomplete="off" type="text" id="form-title" name="title" placeholder="Title"
-                            style="display: none">
+        <input
+          v-model="botmsg"
+          autocomplete="off"
+          type="text"
+          id="form-title"
+          name="title"
+          placeholder="Title"
+          style="display: none"
+        />
 
-                         <textarea 
-                            class="form-message" 
-                            name="message" 
-                            :placeholder="this.$t('contact-form-message')" 
-                            v-model="message"
-                            rows="3"
-                            style="resize: none;"></textarea>
-                     </div>
-                     <button type="submit" class="btn-form"> {{ $t('contact-form-button') }} </button>
-                     <!-- <Button 
+        <textarea
+          class="form-message"
+          name="message"
+          :placeholder="this.$t('contact-form-message')"
+          v-model="message"
+          rows="3"
+          style="resize: none"
+        ></textarea>
+      </div>
+      <button type="submit" class="btn-form">
+        {{ $t("contact-form-button") }}
+      </button>
+      <!-- <Button 
                         :title="this.$t('contact-form-button')" 
                         bColor="--clr-pink-700" 
                         v-type="submit"
                         form="contact-form"
                         ></Button> -->
-                 </form>
+    </form>
 
-                 <img class="contact-blob-nod" src="@/assets/img/blob-6d-nodImage-white.png" alt="Blob Contact Nod"/>
-                 
-             </section>
- 
- </template>
+    <img
+      class="contact-blob-nod"
+      src="@/assets/img/blob-6d-nodImage-white.png"
+      alt="Blob Contact Nod"
+    />
+  </section>
+</template>
  <script>
- import Button from '@/components/Button.vue'
-import { file } from '@babel/types';
+import Button from "@/components/Button.vue";
+import { file } from "@babel/types";
 export default {
   methods: {
-     submit(event) {
-        console.log('Step 0')
-        event.preventDefault();
-        console.log('Step 1 complete')
-        try {
-            let body = {
-                name: this.name,
-                email: this.email,
-                company: this.company,
-                message: this.message,
-                botField: this.botmsg,
-            };
-            console.log(body);
-            fetch(this.url, {
-                method: "POST",
-                body: JSON.stringify(body),
-            }).then((response) => {
-            console.log(response.status); });
-            //console.log(request.status.stringify());
-            this.clearForm();
-            alert("Your message has been sent!");
-        } catch (err) {
-            console.log(err);
-            alert("Oops, something went wrong!");
-        }
+    submit(event) {
+      console.log("Step 0");
+      event.preventDefault();
+      console.log("Step 1 complete");
+      try {
+        let body = {
+          name: this.name,
+          email: this.email,
+          company: this.company,
+          message: this.message,
+          botField: this.botmsg,
+        };
+        console.log(body);
+        fetch(this.url, {
+          method: "POST",
+          body: JSON.stringify(body),
+        }).then((response) => {
+          console.log(response.status);
+        });
+        //console.log(request.status.stringify());
+        this.clearForm();
+        alert("Your message has been sent!");
+      } catch (err) {
+        console.log(err);
+        alert("Oops, something went wrong!");
+      }
     },
 
     clearForm() {
-    this.name = '';
-    this.email = '';
-    this.company = "";
-    this.message = "";
-    }
+      this.name = "";
+      this.email = "";
+      this.company = "";
+      this.message = "";
+    },
   },
-  data () {
+  data() {
     return {
-        name: '',
-        email: '',
-        company: '',
-        message: '',
-        botmsg: '',
-        url: 'https://contact-form.systemvetardagen.workers.dev/'
-    }
+      name: "",
+      email: "",
+      company: "",
+      message: "",
+      botmsg: "",
+      url: "https://contact-form.systemvetardagen.workers.dev/",
+    };
   },
-    name: 'contact'
-}
+  name: "contact",
+};
 components: {
-    Button
+  Button;
 }
 </script>
 <style>
@@ -112,8 +127,8 @@ components: {
   flex-direction: row;
   background: var(--clr-blue-200);
   justify-content: space-between;
-    padding-right: 0;
-    padding-bottom: 5rem;
+  padding-right: 0;
+  padding-bottom: 5rem;
 }
 
 .contact-form {
@@ -123,28 +138,23 @@ components: {
   align-self: flex-start;
 
   border-radius: nullpx;
-   text-align: left;
+  text-align: left;
 
-/* Black/900 */
-   color: #303030;
+  /* Black/900 */
+  color: #303030;
 
+  /* Inside auto layout */
+  flex: none;
+  order: 0;
+  flex-grow: 0;
 
-/* Inside auto layout */
-   flex: none;
-   order: 0;
-   flex-grow: 0;
-
-   padding-top: 10rem;
-
+  padding-top: 10rem;
 }
 
 .inputs {
   display: flex;
   flex-direction: column;
-
-
 }
-
 
 @media (min-width: 1024px) {
   .inputs {
@@ -157,9 +167,7 @@ components: {
   .contact-blob-nod {
     padding-top: 10rem;
     visibility: visible;
-    
-    
-    }
+  }
 }
 input,
 textarea {
@@ -175,21 +183,19 @@ textarea {
   border: none;
 }
 
-input {
-    height: 2rem;
-}
-
 input:focus,
 textarea:focus {
   /* border-color: var(--clr-blue); */
   outline: none;
 }
 
-input::-moz-placeholder, textarea::-moz-placeholder {
+input::-moz-placeholder,
+textarea::-moz-placeholder {
   color: var(--clr-grey-500);
 }
 
-input:-ms-input-placeholder, textarea:-ms-input-placeholder {
+input:-ms-input-placeholder,
+textarea:-ms-input-placeholder {
   color: var(--clr-grey-500);
 }
 
@@ -205,39 +211,31 @@ textarea::placeholder {
 }
 
 .btn-form {
-    /* background-color: var(--clr-blue); */
-    font-weight: 500;
+  /* background-color: var(--clr-blue); */
+  font-weight: 500;
 
-    /* color: var(--clr-white); */
-    border: none;
+  /* color: var(--clr-white); */
+  border: none;
 
-    text-align: center center;
-    text-decoration: none;
-    display: inline-block;
-    font-size: 1.22rem;
-    /* margin-top: 2rem; */
-    padding:0.4em 2.5em;
-    border-radius: 0.5rem;
+  text-align: center center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 1.22rem;
+  /* margin-top: 2rem; */
+  padding: 0.4em 2.5em;
+  border-radius: 0.5rem;
 
-    background: var(--clr-pink-700);
-    color: var(--clr-white);
-
+  background: var(--clr-pink-700);
+  color: var(--clr-white);
 }
 
 @media (max-width: 1024px) {
-.contact-blob-nod {
+  .contact-blob-nod {
     /* padding-top: 20rem;
     padding-right: 20rem; */
     /* overflow: auto;
     padding-top: 20rem; */
     visibility: hidden;
-   
+  }
 }
-
-}
-
-
-
-
-
 </style>
