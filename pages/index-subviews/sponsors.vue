25cf21cda12a9aa88f304d3e86d0dcd194a83709
olehoef <ole.hoefener@t-online.de>
2023-01-25 12:50:21 +0100

    grid_upload-img

--- pages/index-subviews/sponsors.vue
+++ pages/index-subviews/sponsors.vue
@@ -3,7 +3,11 @@
         <div class="sponsor-content">
             <h2 style="color: var(--clr-blue-700);"> {{$t('sponsors')}}</h2>
 
-            <h3>Coming soon...</h3>
+            <div class="sponsor-grid">
+                <div class="sponsor-logo" v-for="logo in logos" :key="logo.name">
+                    <img v-bind:src=" '@/assets/img/sponsor-logos/logo_' + logo.img" v-bind:alt= logo.name >
+                </div>
+            </div>
 
         </div>
         
@@ -15,6 +19,18 @@
     import Button from '@/components/Button.vue'
 
     export default {
+  data () {
+    return {
+        logoPath: '@/assets/img/sponsor-logos/logo_',
+        logos: [{name: 'Capgemini', img: 'capgemini.png'}, 
+                {name: 'Sweco', img:'sweco_black.png'},
+                {name: 'Innofactor', img: 'innofactor.png'},
+                {name: 'Handelsbanken', img: ''},
+                {name: 'Cygni', img: 'cygni.png'},
+                {name: 'Zimply Innovation', img: 'zimply.png'}
+            ]
+    }
+  },
         name: 'heroSection'
     }
     components: {
@@ -24,7 +40,7 @@
 </script>
 <style>
     .sponsors {
-        height: 100vh;
+        height: auto;
         display: flex;
         justify-content: center;
         padding: 5%;
@@ -32,6 +48,23 @@
     }
 
     .sponsor-conent {
+        display: flex;
+        justify-content: center;
     
+    }
+
+    .sponsor-grid {
+        display: grid;
+        grid-template-columns: 50% 50%;
+        grid-gap: 1rem;
+
+    }
+
+    .sponsor-logo {
+        border: solid;
+        height: 10rem;
+        width: 10rem;
+
+
     }
 </style>
