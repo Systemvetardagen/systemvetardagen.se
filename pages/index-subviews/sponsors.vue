16fb4a7a9cdceb9590a9434216cd458637f4a828
olehoef <ole.hoefener@t-online.de>
2023-01-28 14:46:32 +0100

    grid_implement
    grid was hard coded. Logos were updated and aligned properly.

--- pages/index-subviews/sponsors.vue
+++ pages/index-subviews/sponsors.vue
@@ -3,10 +3,54 @@
         <div class="sponsor-content">
             <h2 style="color: var(--clr-blue-700);"> {{$t('sponsors')}}</h2>
 
-            <div class="sponsor-grid">
+            <!-- <div class="sponsor-grid">
                 <div class="sponsor-logo" v-for="logo in logos" :key="logo.name">
-                    <img v-bind:src=" '@/assets/img/sponsor-logos/logo_' + logo.img" v-bind:alt= logo.name >
+                    <img v-bind:src=" '/_nuxt/assets/img/sponsor-logos/logo_' + logo.img" v-bind:alt= logo.name >
                 </div>
+            </div> -->
+
+            <div class="sponsor-grid">
+                <div class="sponsor-logo">
+                    <img 
+                        src="@/assets/img/sponsor-logo/logo_capgemini.png" 
+                        alt="Capgemini">
+                </div>
+                <div class="sponsor-logo sweco" >
+                    <img 
+                        src="@/assets/img/sponsor-logo/logo_sweco_black.png" 
+                        alt="Sweco">
+                </div>
+                <div class="sponsor-logo">
+                    <img 
+                        src="@/assets/img/sponsor-logo/logo_handelsbanken.png" 
+                        alt="Handelsbanken">
+                </div>
+                <div class="sponsor-logo">
+                    <img 
+                        src="@/assets/img/sponsor-logo/logo_innofactor.png" 
+                        alt="Innofactor">
+                </div>
+                <div class="sponsor-logo">
+                    <img 
+                        src="@/assets/img/sponsor-logo/logo_cygni.png" 
+                        alt="Cygni">
+                </div>
+                <div class="sponsor-logo">
+                    <img 
+                        src="@/assets/img/sponsor-logo/logo_zimply.png" 
+                        alt="Zimply">
+                </div>
+                <div class="sponsor-logo">
+                    <img 
+                        src="@/assets/img/sponsor-logo/logo_accenture.png" 
+                        alt="Accenture">
+                </div>
+                <!-- <div class="sponsor-logo">
+                    <img 
+                        src="" 
+                        alt="FRA">
+                </div> -->
+
             </div>
 
         </div>
@@ -47,24 +91,57 @@
 
     }
 
-    .sponsor-conent {
+    .sponsor-content {
         display: flex;
         justify-content: center;
+        flex-direction: column;
+        align-items: center;
+        align-content: center;
     
+    }
+    .sponsor-conent > h2 {
+        align-self: center;
     }
 
     .sponsor-grid {
         display: grid;
         grid-template-columns: 50% 50%;
         grid-gap: 1rem;
+        align-items: center;
+        align-content: center;
 
     }
 
     .sponsor-logo {
-        border: solid;
-        height: 10rem;
+        border: none;
+        /* background: var(--clr-pink-200); */
+        height: 7rem;
         width: 10rem;
-
+        display: flex;
+        align-items: center;
+        border-radius: 1rem;
+        justify-content: center;
 
     }
+
+    .sponsor-logo > img {
+        width: 90%;
+        color: black;
+        /* border: solid; */
+        
+    }
+    .sweco {
+        padding-bottom: 1.5rem;
+    }
+
+    @media (min-width: 1024px) {
+        .sponsor-grid {
+        
+        grid-template-columns: 30% 30% 30%;
+        grid-gap: 2rem;
+        
+
+    }
+	  }
+    
 </style>
