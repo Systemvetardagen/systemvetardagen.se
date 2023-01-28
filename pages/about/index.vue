99903a29a895c8a59bc3bff2dee4b47d2e9bbd3b
Joakim Wirén <47276887+j-wiren@users.noreply.github.com>
2023-01-28 18:05:51 +0100

    More updates to about page

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,11 +1,24 @@
 <template>
   <main>
     <section v-if="posts">
-      <h1 class="title">Company</h1>
-      <div>
+      <h1 class="title">About</h1>
+      <div class="leader-cards">
         <div class="leader-card" v-for="post of posts" :key="post.slug">
-          <h3 class="name">{{ post.name }}</h3>
-          <p>{{ post.role }}</p>
+          <div>
+            <h3 class="name">{{ post.name }}</h3>
+            <p>{{ post.role }}</p>
+          </div>
+          <div>
+            <a v-if="post.linkedin_link" :href="post.linkedin_link">LinkedIn</a>
+            <a v-if="post.portfolio_link" :href="post.portfolio_link"
+              >Link to Portfolio</a
+            >
+          </div>
+          <p v-if="post.about_you">{{ post.about_you }}</p>
+          <div>
+            <p>DISK-email: {{ post.disk_mail }}</p>
+            <p>Personal email: {{ post.personal_mail }}</p>
+          </div>
         </div>
       </div>
     </section>
@@ -23,14 +36,35 @@ export default {
     }
     return { posts };
   },
+  computed: {
+    showEnglishMessage() {
+      return this.$i18n.locale === "sv";
+    },
+  },
 };
 </script> 
 
 <style scoped>
+/* TEMPORARY*/
+section {
+  margin-top: 3rem;
+}
+
+.name {
+  color: var(--clr-blue-900);
+}
+.leader-cards {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
+}
 .leader-card {
-  padding: 1rem;
-  border: solid 0.25rem var(--clr-grey-900);
-  border-radius: 0.5rem;
-  min-width: 16rem;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  padding: 2rem;
+  background-color: var(--clr-blue-100);
+  border-radius: 1rem;
+  width: 24rem;
 }
 </style>
