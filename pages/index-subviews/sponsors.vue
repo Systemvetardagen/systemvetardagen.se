b7c6b41ee3192f54f15fd4e0a2ed9fa036c9c71b
Joakim Wirén <47276887+j-wiren@users.noreply.github.com>
2023-01-29 12:42:37 +0100

    Implement blob placement system (#78)

--- pages/index-subviews/sponsors.vue
+++ pages/index-subviews/sponsors.vue
@@ -1,37 +1,38 @@
 <template>
-    <div class="sponsors">
-        <div class="sponsor-content">
-            <h2 style="color: var(--clr-blue-700);"> {{$t('sponsors')}}</h2>
-
-            <h3>Coming soon...</h3>
-
-        </div>
-        
+  <div class="sponsors">
+    <div class="sponsor-content">
+      <h2 style="color: var(--clr-blue-700)">{{ $t("sponsors") }}</h2>
+      <h3>Coming soon...</h3>
     </div>
-
+    <Blob type="5-b" height="30rem" class="blobby-thing" />
+  </div>
 </template>
 
 <script>
-    import Button from '@/components/Button.vue'
+import Button from "@/components/Button.vue";
+import Blob from "@/components/Blob.vue";
 
-    export default {
-        name: 'heroSection'
-    }
-    components: {
-        Button
-    }
-
+export default {
+  name: "heroSection",
+};
+components: {
+  Button, Blob;
+}
 </script>
 <style>
-    .sponsors {
-        height: 100vh;
-        display: flex;
-        justify-content: center;
-        padding: 5%;
+.sponsors {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  padding: 5%;
+}
 
-    }
+.sponsor-content {
+  z-index: 100;
+}
 
-    .sponsor-conent {
-    
-    }
+.blobby-thing {
+  left: 0;
+  transform: translateX(-50%);
+}
 </style>
