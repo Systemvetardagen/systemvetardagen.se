80dd76d707afb9098f73bbe910956a61092ab13e
aakruti27 <52568409+aakruti27@users.noreply.github.com>
2023-01-30 12:40:55 +0100

    image+button, student node

--- pages/index-subviews/studentnode.vue
+++ pages/index-subviews/studentnode.vue
@@ -1,18 +1,21 @@
 <template>
     <section>
-        <div class="studentnode">
-                <h2 style=" color:var(--clr-blue-600); text-align: center;">{{ $t('book_session') }}</h2>
-                <p>{{$t('student_node_intro')}}</p>
-                <p>{{$t("student_node_howtoapply")}}</p>
-                <p><ul>
-                    <li>{{ $t('student_node_step1') }}<a style="color: var(--clr-blue-600);" href=https://mitt.systemvetardagen.se> {{ $t('student_node_link') }}</a></li>
-                    <li>{{ $t('student_node_step2') }}</li>
-                    <li>{{ $t('student_node_step3') }}</li>
-                    <li>{{ $t('student_node_step4') }}</li>
-                    <li>{{ $t('student_node_step5') }}</li>
-                    <li>{{ $t('student_node_step6') }}</li>
-                </ul></p>
-
+        <div class="studentnode_main">
+            <img class="SNimage" src="@/assets/img/StudentNodeimage.jpg">
+            <div class="studentnode">
+                    <h2 style=" color:var(--clr-blue-600); text-align: center;">{{ $t('book_session') }}</h2>
+                    <p>{{$t('student_node_intro')}}</p>
+                    <p>{{$t("student_node_howtoapply")}}</p>
+                    <p><ul>
+                        <li>{{ $t('student_node_step1') }}<a style="color: var(--clr-blue-600);" href=https://mitt.systemvetardagen.se> {{ $t('student_node_link') }}</a></li>
+                        <li>{{ $t('student_node_step2') }}</li>
+                        <li>{{ $t('student_node_step3') }}</li>
+                        <li>{{ $t('student_node_step4') }}</li>
+                        <li>{{ $t('student_node_step5') }}</li>
+                        <li>{{ $t('student_node_step6') }}</li>
+                    </ul></p>     
+                    <Button link="https://mitt.systemvetardagen.se/" bColor="--clr-pink-600" tColor="--clr-white">Register Now</Button> 
+            </div>
         </div>
 	</section>
 </template>
@@ -25,17 +28,32 @@ export default {
 </script>
 
 <style>
-.studentnode {
-  display: flex;
-  flex-direction: column;
-  position: relative;
-  text-align: left;
-  height: 100vh;
-  max-width: 45ch;
-  background-repeat: no-repeat;
-  margin-bottom: 0rem;
-  margin-top: 0rem;
-  padding-bottom: 12%;
-  padding-top: 12%;
+.studentnode_main {
+    display: flex;
+    flex-wrap: nowrap;
+    flex-direction: column;
+    justify-content: center;
+    padding: 10% 5%;
+    align-items: flex-start;
+    gap: 2rem;
 }
+.studentnode{
+    max-width: 45ch;
+}
+.SNimage{
+    width:90%; 
+    height: auto;
+    opacity: 0.5;
+}
+@media (min-width: 1024px) {
+		 .studentnode_main {
+            flex-direction: row;
+            padding: 20%;
+            padding-left: 7%;
+            padding-right: 13%;
+         }
+         .studentnode {
+            padding-left: 5%;
+        }
+	  }
 </style>
