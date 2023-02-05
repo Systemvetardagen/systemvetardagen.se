af5699074ba3e02a98ead9b2f8ef50eedcea1d32
Joakim Wirén <47276887+j-wiren@users.noreply.github.com>
2023-02-05 23:16:31 +0100

    faq fix

--- pages/index-subviews/faq.vue
+++ pages/index-subviews/faq.vue
@@ -1,32 +1,34 @@
 <template>
-    <section id="FAQ" class="faq">
-        
-            <h2 style="color:var(--clr-blue-600); align-self: center;">{{ $t('frequently_asked_questions') }}</h2>
-            <div class="faq-question">
-                <h3 style="color: var(--clr-pink-600);">{{ $t('question1') }}</h3>
-                <p>
-                    {{ $t('answer1') }}
-                </p>
-            </div>
-            <div class="faq-question">
-                <h3 style="color: var(--clr-pink-600);">{{ $t('question2') }}</h3>
-                <p>
-                    {{ $t('answer2') }}
-                </p>
-            </div>
-            <div class="faq-question">
-                <h3 style="color: var(--clr-pink-600);">{{ $t('question3') }}</h3>
-                <p>
-                    {{ $t('answer3') }}
-                </p>
-            </div>
-        
-    </section>
+  <section id="FAQ" class="faq">
+    <h2 style="color: var(--clr-blue-600); align-self: center">
+      {{ $t("frequently_asked_questions") }}
+    </h2>
+    <div class="faq-questions">
+      <div class="faq-question">
+        <h3 style="color: var(--clr-pink-600)">{{ $t("question1") }}</h3>
+        <p>
+          {{ $t("answer1") }}
+        </p>
+      </div>
+      <div class="faq-question">
+        <h3 style="color: var(--clr-pink-600)">{{ $t("question2") }}</h3>
+        <p>
+          {{ $t("answer2") }}
+        </p>
+      </div>
+      <div class="faq-question">
+        <h3 style="color: var(--clr-pink-600)">{{ $t("question3") }}</h3>
+        <p>
+          {{ $t("answer3") }}
+        </p>
+      </div>
+    </div>
+  </section>
 </template>
 <script>
 export default {
-    name: 'faq'
-}
+  name: "faq",
+};
 </script>
 <style>
 .faq {
@@ -34,20 +36,22 @@ export default {
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  gap: 2rem;
+}
+
+.faq-questions {
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
 }
 
 .faq-question {
-  margin: 2rem 0;
-  max-width: 95%;
-  
+  max-width: 100%;
 }
 
 @media (min-width: 850px) {
-	.faq-question {
-        max-width: 50%
-    }
-    
+  .faq-questions {
+    max-width: 45ch;
+  }
 }
-
-
 </style>
