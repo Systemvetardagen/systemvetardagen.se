f870d266355819cfe66ee71d2d450456691af64e
Joakim Wirén <47276887+j-wiren@users.noreply.github.com>
2023-02-05 23:47:26 +0100

    Info section styling fixes

--- pages/index-subviews/info.vue
+++ pages/index-subviews/info.vue
@@ -1,7 +1,7 @@
 <template>
     <section>
         <div class="info">
-            <div class="info-first">
+            <div>
                 <h3 style=" color:var(--clr-pink-600); text-align: center;">{{ $t('before_the_fair') }}</h3>
                 <p><ul class="list">
                     <li>{{ $t('before_the_fair_description1') }}</li>
@@ -11,8 +11,8 @@
                 </ul></p>
                 <p>{{ $t('before_the_fair_description5') }}</p>
             </div>
-            <div class="info-second">
-                <h3 style="color:var(--clr-blue-600); text-align: center;">{{ $t('during_the_fair') }}</h3>
+            <div>
+                <h3 style="color:var(--clr-pink-600); text-align: center;">{{ $t('during_the_fair') }}</h3>
                 <p><ul>
                     <li>{{ $t('during_the_fair_description1') }}</li>
                     <li>{{ $t('during_the_fair_description2') }}</li>
@@ -26,43 +26,27 @@
 </template>
 
 <script>
-    import Button from '@/components/Button.vue'
+import Button from "@/components/Button.vue";
 
-    export default {
-        name: 'heroSection'
-    }
-    components: {
-        Button
-    }
-
+export default {
+  name: "heroSection",
+};
+components: {
+  Button;
+}
 </script>
 <style>
-    .info{
-        display:flex;
-        flex-direction: column;
+.info {
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 2rem;
 
-        text-align: left;
-        background:  linear-gradient(0deg, rgba(255, 255, 255, 0.4), rgba(255, 255, 255, 0.4));
-        background-position: center;
-        background-size: cover;
-        height: 100vh;
-        background-repeat: no-repeat;
-        margin-bottom: 0rem;
-        margin-top: 0rem;
-    }
-    .info-first{
-        margin-left: 0rem;
-        padding-bottom: 2rem;
-    }
-    .info-second{
-        margin-left: 0rem;
-        padding-bottom:5rem;
-    }
-    .info > div > p > ul {
-        list-style-type: square;
-        
-    }
+  text-align: left;
+}
 
-
-
+.info-text {
+  display: flex;
+  flex-direction: column;
+}
 </style>
