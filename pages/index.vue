53f5759cbab0c78b0208218543889494327a0e58
Joakim Wirén <47276887+j-wiren@users.noreply.github.com>
2023-02-06 15:18:07 +0100

    Hiding studentnode section

--- pages/index.vue
+++ pages/index.vue
@@ -1,45 +1,40 @@
 <!-- use option (alt) + Z to wrap text in vs code -->
 <template>
   <div class="wrapper">
-		<hero/>
-		<sponsors/>
-		<soon/>
-		<studentnode/>
-        <info/>
-		<faq/>
-		<oldCatalog/>
-		<!-- <Button title="See Map" link="map"/> -->
-		<contact/>
-	</div>
+    <hero />
+    <sponsors />
+    <soon />
+    <!-- <studentnode/> -->
+    <info />
+    <faq />
+    <oldCatalog />
+    <!-- <Button title="See Map" link="map"/> -->
+    <contact />
+  </div>
 </template>
 <style>
 </style>
 
 
 <script>
-import hero from '@/pages/index-subviews/hero.vue'
-import soon from '@/pages/index-subviews/soon.vue'
-import studentnode from '@/pages/index-subviews/studentnode.vue'
-import Button from '@/components/Button.vue'
-import info from '@/pages/index-subviews/info.vue'
-import faq from '@/pages/index-subviews/faq.vue'
-import oldCatalog from './index-subviews/oldCatalog.vue'
-import contact from '@/pages/index-subviews/contact.vue'
-import sponsors from './index-subviews/sponsors.vue'
-export default {
-	components: {hero, soon,studentnode, info, faq, oldCatalog, contact, sponsors},
-  	name: 'IndexPage'
-}
+import hero from "@/pages/index-subviews/hero.vue";
+import soon from "@/pages/index-subviews/soon.vue";
+import info from "@/pages/index-subviews/info.vue";
+import faq from "@/pages/index-subviews/faq.vue";
+import oldCatalog from "./index-subviews/oldCatalog.vue";
+import contact from "@/pages/index-subviews/contact.vue";
+import sponsors from "./index-subviews/sponsors.vue";
 
-	components: {
-		hero
-		soon
-		studentnode
-        info
-		faq
-		Button
-		oldCatalog
-		contact
-		sponsors
-  }
+export default {
+  components: {
+    hero,
+    soon,
+    info,
+    faq,
+    oldCatalog,
+    contact,
+    sponsors,
+  },
+  name: "IndexPage",
+};
 </script>
