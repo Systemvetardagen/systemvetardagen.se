2c437497cce6de4706f3266e5dbab9956e8ee4a5
Joakim Wirén <47276887+j-wiren@users.noreply.github.com>
2023-02-08 14:44:53 +0100

    Partial style implementation for companies page

--- pages/companies/index.vue
+++ pages/companies/index.vue
@@ -2,17 +2,21 @@
   <main class="wrapper">
     <section v-if="posts">
       <h1 class="title">Company</h1>
-      <div>
+      <div class="company-cards">
         <div v-for="post of posts" :key="post.slug">
           <div v-if="showEnglishMessage">
-            <li v-if="post.slug === post.title.toLowerCase() + '.sv'">
-              <NuxtLink :to="post.title.toLowerCase() + '.sv'">{{ post.title }}</NuxtLink>
-            </li>
+            <div v-if="post.slug === post.title.toLowerCase() + '.sv'">
+              <NuxtLink :to="post.title.toLowerCase() + '.sv'">
+                <company-card class="company-card" :company="post" />
+              </NuxtLink>
+            </div>
           </div>
 
           <div v-else="showEnglishMessage">
             <li v-if="post.slug === post.title.toLowerCase() + '.en'">
-              <NuxtLink :to="post.title.toLowerCase() + '.en'">{{ post.title }}</NuxtLink>
+              <NuxtLink :to="post.title.toLowerCase() + '.en'">{{
+                post.title
+              }}</NuxtLink>
             </li>
           </div>
         </div>
@@ -22,7 +26,12 @@
 </template>
 
 <script>
+import CompanyCard from "@/components/CompanyCard.vue";
+
 export default {
+  components: {
+    CompanyCard,
+  },
   async asyncData({ $content, error }) {
     let posts;
     try {
@@ -39,3 +48,14 @@ export default {
   },
 };
 </script> 
+<style scoped>
+.company-cards {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  max-width: 64rem;
+}
+.company-card {
+  margin: 1rem;
+}
+</style>
