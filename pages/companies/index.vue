f5a49679a6ae8deb54334d22abafed33c63c1a49
FerminCho <fermin.liljeqvist@gmail.com>
2023-02-08 17:11:38 +0100

    fixed a better solution for filtering between all the swedish and english variants of company pages

--- pages/companies/index.vue
+++ pages/companies/index.vue
@@ -3,16 +3,16 @@
     <section v-if="posts">
       <h1 class="title">{{ $t("companies") }}</h1>
       <div class="company-cards">
-        <div v-for="post of posts" :key="post.slug">
-          <div v-if="showEnglishMessage">
-            <div v-if="post.slug === post.title.toLowerCase() + '.sv'">
-              <NuxtLink :to="post.title.toLowerCase() + '.sv'">
+        <div v-for="post of filterdPosts" :key="post.slug">
+          <!---<div v-if="showEnglishMessage">
+            <div v-if="post.slug === post.title.toLowerCase() + '.sv'">-->
+              <NuxtLink :to="post.slug">
                 <company-card class="company-card" :company="post" />
               </NuxtLink>
             </div>
           </div>
 
-          <div v-else="showEnglishMessage">
+          <!--<div v-else="showEnglishMessage">
             <div v-if="post.slug === post.title.toLowerCase() + '.en'">
               <NuxtLink :to="post.title.toLowerCase() + '.en'">
                 <company-card class="company-card" :company="post" />
@@ -20,7 +20,7 @@
             </div>
           </div>
         </div>
-      </div>
+      </div>-->
     </section>
   </main>
 </template>
@@ -45,7 +45,15 @@ export default {
     showEnglishMessage() {
       return this.$i18n.locale === "sv";
     },
+    filterdPosts(){
+      if(this.showEnglishMessage){
+        return this.posts.filter(e => e.slug.includes("." + this.$i18n.locale))
+      }
+    }
   },
+  methods: {
+
+  }
 };
 </script> 
 <style scoped>
