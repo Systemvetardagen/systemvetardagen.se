56eeaf8d080dcdfa41436f85faa23373d1822ead
Joakim Wirén <47276887+j-wiren@users.noreply.github.com>
2023-02-10 00:42:37 +0100

    Styling for filters

--- pages/companies/index.vue
+++ pages/companies/index.vue
@@ -6,28 +6,92 @@
       <!-- temporary form for filter. Please note that it is now only developed with core feature,
         allPrograms and allPositions are string arrays imported from a hardcoded file
         (extracted from static/admin/config.yml) and did not consider i18n -->
-      <form>
-        <h3>Programs:</h3>
-        <label v-for="program in allPrograms" :key="program">
-          <input type="checkbox" v-model="selectedPrograms" :value="program"/>
-          {{ program }}
-        </label>
-        <h3>Positions:</h3>
-        <label v-for="position in allPositions" :key="position">
-          <input type="checkbox" v-model="selectedPositions" :value="position">
-          {{ position }}
-        </label>
-      </form>
-
+      <div class="filter-paragraph">
+        Showing companies for
+        <span
+          class="dropdown-toggle programs-toggle"
+          @click="programsVisible = !programsVisible"
+          >all programs
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="3"
+            stroke="currentColor"
+            class="chevron"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+            />
+          </svg>
+        </span>
+        <div v-show="programsVisible" class="dropdown-container">
+          <div
+            class="dropdown-program"
+            v-for="program in allPrograms"
+            :key="program"
+          >
+            <input
+              type="checkbox"
+              :name="program"
+              :id="program"
+              :value="program"
+              v-model="selectedPrograms"
+            />
+            <label :for="program">{{ program }}</label>
+          </div>
+        </div>
+        and
+        <span
+          class="dropdown-toggle positions-toggle"
+          @click="positionsVisible = !positionsVisible"
+          >all positions
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="3"
+            stroke="currentColor"
+            class="chevron"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+            />
+          </svg>
+        </span>
+        <div v-show="positionsVisible" class="dropdown-container">
+          <div
+            class="dropdown-positions"
+            v-for="position in allPositions"
+            :key="position"
+          >
+            <input
+              type="checkbox"
+              :name="position"
+              :id="position"
+              :value="position"
+              v-model="selectedPositions"
+            />
+            <label :for="position">{{ position }}</label>
+          </div>
+        </div>
+      </div>
       <div class="company-cards">
         <div v-for="post of filterdPosts" :key="post.slug">
           <!---<div v-if="showEnglishMessage">
             <div v-if="post.slug === post.title.toLowerCase() + '.sv'">-->
           <NuxtLink :to="post.slug">
             <company-card
-            class="company-card"
-            :company="post"
-            v-if="filterOneCondition(post.program, selectedPrograms) && filterOneCondition(post.positions, selectedPositions)"
+              class="company-card"
+              :company="post"
+              v-if="
+                filterOneCondition(post.program, selectedPrograms) &&
+                filterOneCondition(post.positions, selectedPositions)
+              "
             />
           </NuxtLink>
         </div>
@@ -47,15 +111,16 @@
 </template>
 
 <script>
-
 import CompanyCard from "@/components/CompanyCard.vue";
 
 export default {
   data() {
     return {
+      positionsVisible: false,
+      programsVisible: false,
       selectedPrograms: [],
-      selectedPositions: []
-    }
+      selectedPositions: [],
+    };
   },
   components: {
     CompanyCard,
@@ -78,22 +143,62 @@ export default {
     },
   },
   created() {
-    this.allPrograms = require("@/content/filter_data.json").programs
-    this.allPositions = require("@/content/filter_data.json").positions
+    this.allPrograms = require("@/content/filter_data.json").programs;
+    this.allPositions = require("@/content/filter_data.json").positions;
   },
   methods: {
     filterOneCondition(condition, selection) {
       // when no filter selected, display all posts
       if (!selection || !selection.length) {
-        return true
+        return true;
       }
       // else display post whose condition includes any item in selection
-      return selection.some(s => condition.includes(s))
-    }
-  }
+      return selection.some((s) => condition.includes(s));
+    },
+  },
 };
 </script> 
 <style scoped>
+.dropdown-container {
+  position: absolute;
+  padding: 2rem;
+  border-radius: 1rem;
+  background-color: var(--clr-white);
+  margin-top: 1rem;
+  box-shadow: 0 0.25rem 0.5rem #0002;
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+}
+
+.dropdown-toggle {
+  padding: 0.25rem;
+  -moz-user-select: none;
+  user-select: none;
+}
+
+.filter-paragraph {
+  line-height: 200%;
+}
+
+.programs-toggle {
+  background-color: var(--clr-pink-500);
+  display: inline-block;
+}
+
+.positions-toggle {
+  background-color: var(--clr-yellow-500);
+  display: inline-block;
+}
+
+.chevron {
+  width: 1rem;
+  margin-bottom: -0.15rem;
+}
+
+label {
+  font-family: work-sans;
+}
 .company-cards {
   display: flex;
   flex-wrap: wrap;
