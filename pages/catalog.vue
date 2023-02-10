f0c29331b062b228d740125bba9103690d04ad5c
FerminCho <fermin.liljeqvist@gmail.com>
2023-02-10 19:45:52 +0100

    started on fixing catalog page

--- pages/catalog.vue
+++ pages/catalog.vue
@@ -1,6 +1,31 @@
 <template>
   <div class="wrapper">
     <h1>Catalog</h1>
-    <Button link="/companies" bColor="gradient">Companies</Button>
+    <Button link="/companies/" bColor="gradient">Companies</Button>
+    <div v-for="post of filterdPosts" :key="post.slug">
+    
+    </div>
+
   </div>
 </template>
+
+
+<script>
+export default {
+  async asyncData({ $content, error }) {
+    let posts;
+    try {
+      posts = await $content("preview").fetch();
+    } catch (e) {
+      error({ message: "Posts not found" });
+    }
+    return { posts };
+  },
+  computed: {
+    filterdPosts() {
+      return this.posts.filter((e) => e.slug.includes("." + this.$i18n.locale));
+    },
+  },
+}
+
+</script>
