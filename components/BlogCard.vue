4afaa396e9cb605e44137387273df8c3009dd766
Joakim Wirén <47276887+j-wiren@users.noreply.github.com>
2023-02-11 19:01:46 +0100

    Catalog styling + BlogCard

---
+++ components/BlogCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <NuxtLink :to="post.slug">
+    <article>
+      <h3>{{ post.title }}</h3>
+    </article>
+  </NuxtLink>
+</template>
+
+<script>
+export default {
+  props: {
+    post: Object,
+  },
+};
+</script>
