d0f71d59ca142ca97c7bb16b4c2c66ad62744f93
TP15 <57965073+TP15@users.noreply.github.com>
2023-10-09 11:34:49 +0200

    Create user-page.vue
    User-Page first draft

---
+++ pages/user-page.vue
@@ -0,0 +1,39 @@
+<template>
+<div class="wrapper">
+    <h1 class="title">User Page</h1>
+   <!-- <nuxt-img 
+          class="post-img"
+    /> -->
+
+    <div class="userinformation">
+
+
+    </div>
+<div class="contentButtons">
+    <Button :link="localePath('/user-page') + '/'" >Upload Content</Button>
+      <Button :link="localePath('/user-page') + '/'" > Upload Pictures </Button> <!-- Translation needed-->
+      </div>
+</div>
+
+
+</template>
+
+<script>
+
+
+
+
+</script>
+
+
+<style>
+.contentButtons {
+    display: flex;
+    align-items: center;
+}
+.post-img {
+  max-width: 100%;
+  border-radius: 1rem;
+  margin-bottom: 2rem;
+}
+</style>
