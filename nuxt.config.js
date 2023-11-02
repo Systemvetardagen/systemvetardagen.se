ab3aecf8a75f610a503d3ab118bd0f58830f09c9
R3Kr <thefungigg@gmail.com>
2023-11-02 23:19:15 +0100

    preview is now added

--- nuxt.config.js
+++ nuxt.config.js
@@ -155,7 +155,7 @@ export default {
   css: ["@/assets/css/main.css"],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: ['./plugins/preview.client.js'],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
