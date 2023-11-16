d5ca65fd637a60e2bac7fee1a28354f85eeb4e90
R3Kr <thefungigg@gmail.com>
2023-11-16 15:46:36 +0100

    moved the old catalog into the old route

--- pages/companies/old/index.vue
+++ pages/companies/old/index.vue
@@ -141,7 +141,7 @@
           <NuxtLink
             :to="
               localePath({
-                name: 'companies-companies',
+                name: 'companies-old-companies',
                 params: { companies: post.slug.split('.')[0] },
               })
             "
