aed6ae37a1242811316be0db088a3de21bee2438
olehoef <ole.hoefener@t-online.de>
2024-01-14 12:13:22 +0100

    changed styles for extra text using js

--- components/subViews/company/partnerContent.vue
+++ components/subViews/company/partnerContent.vue
@@ -17,6 +17,7 @@
     v-if="post.sponsor_extra_text"
     v-html="markdownToHtml"
     class="extra-text"
+    ref="contentContainer"
     ></div>
     <!-- SPONSOR IMAGES // GALLERY -->
     <div v-if="post.sponsor_images" class="gallery">
@@ -57,11 +58,74 @@
       props: {
           post: Object
       },
+      data() {
+        return {
+          contentChanged: false,
+        }
+      },
       computed: {
         markdownToHtml() {
           return marked.parse(this.post.sponsor_extra_text);
         },
-      }
+      },
+      mounted() {
+      // Watch for changes in sponsor_extra_text and set contentChanged to true
+        this.$watch('post.sponsor_extra_text', () => {
+          this.contentChanged = true;
+        });
+      },
+      watch: {
+        // Watch for changes in contentChanged and apply styles when true
+        contentChanged: {
+          handler() {
+            this.$nextTick(() => {
+              this.applyStyles();
+              // Reset contentChanged after applying styles
+              this.contentChanged = false;
+            });
+          },
+          immediate: true,
+        },
+      },
+    methods: {
+      applyStyles() {
+        // Access the DOM element using the ref
+        const contentContainer = this.$refs.contentContainer;
+
+        if (contentContainer) {
+          // Apply styles to specific elements within the dynamically injected content
+          const h1Elements = contentContainer.querySelectorAll('h1');
+          const h2Elements = contentContainer.querySelectorAll('h2');
+          const h3Elements = contentContainer.querySelectorAll('h3');
+          const liElements = contentContainer.querySelectorAll('li');
+          const ulElements = contentContainer.querySelectorAll('ul');
+          const pElements = contentContainer.querySelectorAll('p')
+          h1Elements.forEach((h1) => {
+            h1.style.fontSize = '2rem';
+            h1.style.margin = '1.5rem 0'
+            // Add more styles or logic as needed
+          });
+          h2Elements.forEach((h2) => {
+            h2.style.fontSize = '1.5rem';
+            h2.style.marginTop = '1.5rem'
+          });
+          h3Elements.forEach((h3)=> {
+            h3.style.fontSize = '1rem';
+            h3.style.marginTop = '1rem';
+            h3.style.marginBottom = '0'
+          });
+          liElements.forEach((li)=> {
+            li.style.margin = '0.2rem 0'
+          });
+          ulElements.forEach((ul)=> {
+            ul.style.paddingLeft = '2rem'
+          });
+          pElements.forEach((p)=> {
+            p.style.margin = '0.2rem 0 0.7rem 0 '
+          })
+        }
+      },
+    }
   }
 
 </script>
@@ -71,6 +135,7 @@
 .extra-text {
   margin-top: 4rem;
 }
+
 
 /* YouTube Video */
 .video {
