2721e27f4a684a978c9eb2a74dd30ac2967a554f
olehoef <ole.hoefener@t-online.de>
2024-02-02 16:42:47 +0100

    removing old classes

--- components/subViews/company/banner.vue
+++ components/subViews/company/banner.vue
@@ -34,36 +34,9 @@
     .banner-img {
     width: 100%;
     object-fit: cover;
+    overflow: hidden;
     }
 
-    .banner-bar {
-    position: absolute;
-    bottom: calc(clamp(2px, 1vw, 10px) * -1);
-    left: 0;
-    width: 100%;
-    height: clamp(2px, 1vw, 10px);
-    background: linear-gradient(
-        90deg,
-        var(--clr-blue-600),
-        var(--clr-pink-600),
-        var(--clr-yellow-600)
-    );
-    border-radius: 0 0 1rem 1rem;
-    }
-
-    .email {
-    font-family: work-sans;
-    }
-
-    .post-location {
-    padding-bottom: clamp(0.2rem, 1vw, 0.5rem);
-    border-bottom: solid 2px var(--clr-white);
-    }
-    .post-tag {
-    align-self: flex-end;
-    padding-top: clamp(0.2rem, 1vw, 0.5rem);
-    color: white;
-    }
 
 
 
