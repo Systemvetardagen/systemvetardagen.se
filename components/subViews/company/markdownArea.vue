d95c99fd8ce818942f24b4251f406fd26ac99b65
Jacob Reinikainen Lindström <80760206+jazzjacob@users.noreply.github.com>
2024-02-02 13:37:14 +0100

    styling fine tuning

--- components/subViews/company/markdownArea.vue
+++ components/subViews/company/markdownArea.vue
@@ -1,7 +1,7 @@
 <template>
     <div v-html="htmlContent" :id="$style.htmlContainer"></div>
   </template>
-  
+
   <script>
   export default {
     props: {
@@ -9,8 +9,12 @@
     }
   };
   </script>
-  
+
   <style module>
+    #htmlContainer {
+      letter-spacing: 0;
+      line-height: 1.5;
+    }
     #htmlContainer > h1 {
         font-size: 2rem;
         margin: 1.5rem 0;
@@ -18,6 +22,7 @@
     #htmlContainer > h2 {
         font-size: 1.5rem;
         margin-top: 1.5rem;
+        margin-bottom: 0rem;
     }
     #htmlContainer > h3 {
         font-size: 1rem;
@@ -26,11 +31,13 @@
     }
     #htmlContainer > li {
         margin: 0.2rem 0;
+        line-height: 1.5;
     }
     #htmlContainer > ul {
-        padding-left: 2rem;
+        /*padding-left: 2rem;*/
     }
     #htmlContainer > p {
-      margin: 0.2rem 0 0.7rem 0
+      margin: 0.2rem 0 0.7rem 0;
+      line-height: 1.5;
     }
   </style>
