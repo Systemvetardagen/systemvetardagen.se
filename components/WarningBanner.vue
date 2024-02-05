eb97eafeb942e484ef66dbeb2d3437f41cac5923
olehoef <ole.hoefener@t-online.de>
2024-02-05 20:24:39 +0100

    Warning banner

---
+++ components/WarningBanner.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="banner">
+        <p>
+            <slot></slot>
+        </p>
+    </div>
+</template>
+<script>
+</script>
+<style scoped>
+.banner {
+    width: 100%;
+    background-color: var(--clr-grey-800);
+    color: var(--clr-pink-500);
+    text-align: center;
+    padding: 0.5rem;
+    position: fixed;
+    z-index: 2;
+    font-weight: 450;
+}
+
+</style>
