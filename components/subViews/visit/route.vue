531aefd1147ff733645bfc003824e3a5b5ed35dd
olehoef <ole.hoefener@t-online.de>
2024-02-15 19:10:25 +0100

    gmap iframe

--- components/subViews/visit/route.vue
+++ components/subViews/visit/route.vue
@@ -1,5 +1,7 @@
 <template>
-    <div>
-        
+    <div class="wrapper">
+        <div class="gmap">
+            <iframe width="600" height="350" style="border:0" loading="lazy" allowfullscreen src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJK5qoUOyeX0YRqd75dWHaTTI&key=AIzaSyC8vKu7a8jiOo0WvfdyRum94cZokGK2yDA&zoom=10"></iframe>
+        </div>
     </div>
 </template>
--- pages/visit/index.vue
+++ pages/visit/index.vue
@@ -1,5 +1,12 @@
 <template>
     <div>
-        
+        <Route/>
     </div>
 </template>
+<script>
+    import Route from '@/components/subViews/visit/route.vue'
+    export default {
+        components: {
+            Route        }
+    }
+</script>
