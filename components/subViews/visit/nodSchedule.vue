7f8215d34e429842b121516abf2e22d00905f6d4
Jacob Reinikainen Lindström <80760206+jazzjacob@users.noreply.github.com>
2024-02-20 12:00:04 +0100

    akavia added to lectures

--- components/subViews/visit/nodSchedule.vue
+++ components/subViews/visit/nodSchedule.vue
@@ -18,6 +18,7 @@
         data() {
             return {
                 lectures: [
+                    {time: "10:20—10:50", company: "Akavia"},
                     {time: "11:00—11:40", company: "Truesec", title: "From Infection to Encryption: A Deep Dive into Threat Actors Malicious Code"},
                     {time: "12:00—12:30", company: "Accenture"},
                     {time: "13:00—13:30", company: "Zimply"},
